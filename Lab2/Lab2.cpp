28949de82445725caa2048157db6b3c2669136ae
vovarishev <41824455+vovarishev@users.noreply.github.com>
2021-03-05 16:27:18 +0700

    Update Lab2.cpp

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -1,20 +1,19 @@
-﻿#include <iostream>
+#include "stdafx.h"
+#include <iostream>
 
 using namespace std;
 
 int GetN();
 void GetSize(int&, int&);
-void NewMatrix(double**, int&, int&);
-void ManualMatrix(double** ,int&, int&);
-void MemClear(double**, int&);
+void NewMatrix(double**&, int&, int&);
+void ManualMatrix(double**, int&, int&);
+void MemClear(double**&, int&);
 
-int main(){
+int main() {
 	cout << "Wellcome! This programm can solve the system of equations.\n";
-	int N = 0, M= 0;
+	int N = 2, M = 2;
 	double** a;
-	a = new double* [N];
-	MemClear(a, N); //почистить память
-    
+
 	char K;
 	do {
 		cout << "\n1 - Make matrix with manual entered parametres;\nq - Exit.\n";
@@ -23,13 +22,13 @@ int main(){
 		{
 		case '1':
 			NewMatrix(a, N, M);
-			ManualMatrix(a,N, M);
+			ManualMatrix(a, N, M);
 			break;
 		case '2':
-			
+
 			break;
 		case '3':
-			
+
 			break;
 		case '4':
 
@@ -45,52 +44,60 @@ int main(){
 			}
 		}
 	} while (K != 'q');
-	//MemClear(a, N); //почистить память
-    return 0;
+	MemClear(a, N); //почистить память
+	return 0;
 }
 
-void MemClear(double** a, int& N) {
+void MemClear(double**& a, int& N) {
 	for (int i = 0; i < N; i++) {
-		delete[]a[i];
+		delete[] a[i];
+		//cout << "OMG!!! Das work" << endl;//debug !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	}
 	delete[] a;
 	a = NULL;
 	cout << "Sucsesfully clear memory" << endl;
 }
 
-int GetN(){
-    float n;
-    do {
-        cin >> n;
-        if (n <= 0 || n - (int)n != 0)
-            cout << "!--- Error, try int number..." << endl;
-    } while (n <= 0 || n - (int)n != 0);
-    return n;
+int GetN() {
+	float n;
+	do {
+		cin >> n;
+		if (n <= 0 || n - (int)n != 0)
+			cout << "!--- Error, try int number..." << endl;
+	} while (n <= 0 || n - (int)n != 0);
+	return n;
 }
 
-void GetSize(int& N, int& M){
+void GetSize(int& N, int& M) {
 	cout << "Enter size of two-dimensional matrix: \n";
-    N = GetN(); M = GetN();
-	N--; M--;
+	N = GetN(); M = GetN();
 }
 
-void NewMatrix(double** a, int& N, int& M){
-    GetSize(N, M);
-    cout << "Matrix have size: " << N+1 << "x" << M+1 << endl;
-    a = new double* [N];
-    for (int i = 0; i < N; i++) {
-        a[i] = new double[M];
-    }
-    cout << "Sucsesfully get memory for matrix" << endl;
+void Preparator(double **&a, int &N, int &M){
+	cout << "Matrix have size: " << N + 1 << "x" << M + 1 << endl;
+	a = new double*[N];
+	for (int i = 0; i < N; i++) {
+		a[i] = new double[M];
+	}
+	cout << "Sucsesfully get memory for matrix" << endl;
 }
 
-void ManualMatrix(double** a,int& N, int& M) {
+void NewMatrix(double**& a, int& N, int& M) {
+	GetSize(N, M);
+	cout << "Matrix have size: " << N  << "x" << M  << endl;
+	a = new double*[N];
+	for (int i = 0; i < N; i++) {
+		a[i] = new double[M];
+	}
+	cout << "Sucsesfully get memory for matrix" << endl;
+}
+
+void ManualMatrix(double** a, int& N, int& M) {
 	for (int i = 0; i <= N; i++) {
 		for (int j = 0; j <= M; j++) {
 			cout << "\nEnter [" << i << "][" << j << "] element: ";
 			cin >> a[i][j];
 			cout << a[i][j];
 		}
-		cout << "OMG!!! Das work" << endl;//debug !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	}
 }
