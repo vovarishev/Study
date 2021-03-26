d0d8e2f633a0f43d16bc63736bd2e1f41e4a9ca9
vovarishev <41824455+vovarishev@users.noreply.github.com>
2021-03-26 11:05:24 +0700

    Add files via upload

---
+++ Lab3/Lab3.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+bool GetList();
+
+int main()
+{
+    struct StackChart {//Инициализация стека
+        struct Node {
+            string name;
+            int amount;
+            int priceCost;
+            int priceSell;
+            Node* prev;
+        };
+        Node* Top = NULL;
+        int Count;
+    };
+    GetList();
+}
+
+bool GetList() {
+    string str;
+    int i = 0;
+    ifstream f("List.txt");
+    if (!f) {
+        cout << "! --- Error --- ! Can't open file \"List.txt\" \n";
+        return false;
+    }
+    while (!f.eof()){
+        getline(f, str);
+        i++;
+    }
+    f.close();
+    cout <<"Amount of strings in file: "<< i;
+    return true;
+}
